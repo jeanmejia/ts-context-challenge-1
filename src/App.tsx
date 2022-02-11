1ee9e11da0bc4aae7d686cc44854f38aa079ad83
jeanpierre <jean.mejia.rev@protonmail.com>
2022-02-11 12:39:53 -0500

    feat: add useReducer with addCar, updateCar, removeCar functions

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,21 @@ const ThemeSwitcher = () => {
 
 const MainLayout = () => {
   const { isLightMode, light, dark } = useThemeContext();
+  const { addCar, updateCar, removeCar } = useUserContext();
   const { name } = useUserContext();
 
   const theme = isLightMode ? light : dark;
 
   return (
-    <div className="main" style={{ background: theme.bg }}>
-      <div style={{ color: theme.text }}>{`Hello ${name}`}</div>
+    <div>
+      <div className="main" style={{ background: theme.bg }}>
+        <div style={{ color: theme.text }}>{`Hello ${name}`}</div>
+      </div>
+      <div style={{display:'flex', flexDirection:'row', justifyContent: 'space-evenly'}}>
+        <button onClick={addCar}>Add Car</button>
+        <button onClick={updateCar}>Update Car</button>
+        <button onClick={removeCar}>Remove Car</button>
+      </div>
     </div>
   );
 };
--- src/Context/UserInfo.tsx
+++ src/Context/UserInfo.tsx
@@ -1,15 +1,36 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 interface Person {
   name: string;
   lastName: string;
   isLoggedIn: boolean;
-  cars: [{ brand: string; color: string }, { brand: string; color: string }];
+  cars: Car[];
   toggleLogin?: () => void;
+  addCar?: () => void;
+  updateCar?: () => void;
+  removeCar?: () => void;
 }
+
+interface Car {
+  id: number;
+  brand: string;
+  color: string;
+}
+
 type Props = {
   children: React.ReactNode;
 };
+
+type Action = {
+  type: CarActionKind;
+  payload: Car;
+};
+
+enum CarActionKind {
+  add = "ADD_CAR",
+  update = "UPDATE_CAR",
+  remove = "REMOVE_CAR",
+}
 
 export const UserContext = createContext<Person | null>(null);
 
@@ -19,10 +40,12 @@ const initialState: Person = {
   isLoggedIn: false,
   cars: [
     {
+      id: 1,
       brand: "Toyota",
       color: "Blue",
     },
     {
+      id: 2,
       brand: "Nissan",
       color: "Red",
     },
@@ -37,20 +60,59 @@ export const useUserContext = () => {
   return context;
 };
 
-const UserContextProvider = ({ children }: Props) => {
-  const [userInfo, setUserInfo] = useState(initialState);
-
-  const toggleLogin = () => {
-    setUserInfo((state) => {
+function carReducer(state: Person, action: Action) {
+  switch (action.type) {
+    case CarActionKind.add:
       return {
         ...state,
-        isLoggedIn: !state.isLoggedIn,
+        cars: [...state.cars, action.payload],
       };
+    case CarActionKind.update:
+      return {
+        ...state, cars: state.cars.map((car) => {
+          if (car.id === action.payload.id){
+            return action.payload;
+          }
+          return car
+        })
+      }
+    case CarActionKind.remove:
+      return {
+        ...state,
+        cars: state.cars.filter((car) => car.id !== action.payload.id),
+      };
+  }
+}
+
+const UserContextProvider = ({ children }: Props) => {
+  const [userInfo, dispatch] = useReducer(carReducer, initialState);
+
+  const addCar = () => {
+    dispatch({
+      type: CarActionKind.add,
+      payload: { id: 3, brand: "Chevrolet", color: "Yellow" },
     });
+    console.log(userInfo.cars);
+  };
+
+  const updateCar = () => {
+    dispatch({
+      type: CarActionKind.update,
+      payload: { id: 1, brand: "Toyota", color: "Green"}
+    })
+  }
+  console.log(userInfo.cars);
+
+  const removeCar = () => {
+    dispatch({
+      type: CarActionKind.remove,
+      payload: { id: 1, brand: "Toyota", color: "Blue" },
+    });
+    console.log(userInfo.cars);
   };
 
   return (
-    <UserContext.Provider value={{ ...userInfo, toggleLogin }}>
+    <UserContext.Provider value={{ ...userInfo, addCar, removeCar, updateCar }}>
       {children}
     </UserContext.Provider>
   );
